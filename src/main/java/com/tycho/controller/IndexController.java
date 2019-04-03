9091ad58a0d33a7b4110eeb851650f1a23376ba8
luliang <765296872@qq.com>
2019-04-03 15:33:36 +0800

    init

---
+++ src/main/java/com/tycho/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.tycho.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Component
+@Controller
+public class IndexController {
+    @RequestMapping(value = "/index",method= RequestMethod.GET)
+    public String index(){
+    	System.out.println("spring is working");
+        return "index";
+    }
+}
